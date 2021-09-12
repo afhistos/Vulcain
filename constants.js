0d7e289b241d8ec17a3312761b320e7b994772be
afhistos <32820096+afhistos@users.noreply.github.com>
2021-09-12 02:32:56 +0200

    Change the version to something more logical

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const appVersion = "1.1.0-alpha";
+const appVersion = "0.1.0-alpha";
 const wsUrl = 'ws://193.168.147.112:44444';
 console.warn("Ignore the next error. 'module.exports' is for NodeJS imports and is not recognized by the JS parser, but is needed to pass infos to the main process");
 //For node.js "require" function
